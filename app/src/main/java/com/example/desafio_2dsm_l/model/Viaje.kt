3fe3d16daddd75eb9931efd3bfe12069f4b62f23
harold.quintanilla <hquintanila@gmail.com>
2026-09-09 11:48:46 -0600

    feat: Crear Modelo de Datos Viaje Para Ofertas de la Agencia

---
+++ app/src/main/java/com/example/desafio_2dsm_l/model/Viaje.kt
@@ -0,0 +1,12 @@
+package com.example.desafio_2dsm_l.model
+
+data class Viaje(
+    var id: String = "",
+    var titulo: String = "",
+    var descripcion: String = "",
+    var precio: Double = 0.0,
+    var duracion: String = "",      // Ejemplo: "3 Días / 4 Noches"
+    var ubicacion: String = "",     // Ejemplo: "Cancún, México"
+    var imagenUrl: String = "",     // URL guardada en Firebase Storage o servidor web
+    var disponible: Boolean = true
+)
