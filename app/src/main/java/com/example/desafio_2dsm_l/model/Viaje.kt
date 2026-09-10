c40fb9e7f2abee6ee2a8326a546a8afe2e302d4f
harold.quintanilla <hquintanila@gmail.com>
2026-09-10 11:27:13 -0600

    feat: Agregar Campo imagenResId al Modelo Viaje para Soporte de Recursos Locales

--- app/src/main/java/com/example/desafio_2dsm_l/model/Viaje.kt
+++ app/src/main/java/com/example/desafio_2dsm_l/model/Viaje.kt
@@ -7,6 +7,7 @@ data class Viaje(
     var precio: Double = 0.0,
     var duracion: String = "",      // Ejemplo: "5 Días / 4 Noches"
     var ubicacion: String = "",     // Ejemplo: "Cancún, México"
-    var imagenUrl: String = "",     // URL guardada en Firebase Storage o servidor web
+    var imagenUrl: String = "",     // URL remota de Firebase Storage o web
+    var imagenResId: Int = 0,       // ID del recurso local (Ejemplo: R.drawable.cancun1)
     var disponible: Boolean = true
 )
